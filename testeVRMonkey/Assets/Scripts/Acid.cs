9113caab778274b08c32e3e47ae83d61803aeb08
danielhartax <eumesmo@danielhartax.com>
2021-06-12 23:26:01 -0300

    ajuste no box de hide, cloak e ajustes no cenario para os fossos de acido com a configuracao dos mesmos. navmash refeita

---
+++ testeVRMonkey/Assets/Scripts/Acid.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Acid : MonoBehaviour
+{
+    public GameObject splash;
+
+    public AudioSource som;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.tag == "Player")
+        {
+            GameObject s = Instantiate(splash, col.transform.position, Quaternion.Euler(new Vector3(-90f, 0f, 0f)));
+            som.Play();
+            Destroy(s, 3f);
+            StealthPlayerController colCharacter = col.GetComponent<StealthPlayerController>();
+            colCharacter.SetEnergy(0.5f);
+        }
+
+    }
+}

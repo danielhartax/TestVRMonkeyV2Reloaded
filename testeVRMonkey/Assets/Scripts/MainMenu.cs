aeed327684ffa1cb87d4cf9ce132f10d71662895
danielhartax <eumesmo@danielhartax.com>
2021-06-13 03:46:59 -0300

    post processing e edicao da tela de inicio e fim. pronto para building final

--- testeVRMonkey/Assets/Scripts/MainMenu.cs
+++ testeVRMonkey/Assets/Scripts/MainMenu.cs
@@ -6,15 +6,19 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour {
 
+    public GameObject loadingVideo;
 	public void StartGame()
     {
         HUDManager.instance.FadeInBlack(1.0f);
         StartCoroutine(startGameRoutine());
+        
     }
 
     IEnumerator startGameRoutine()
     {
         yield return new WaitForSeconds(1.5f);
-        SceneManager.LoadScene(1);
+        loadingVideo.SetActive(true);
+        yield return new WaitForSeconds(1.5f);
+        SceneManager.LoadSceneAsync(1, LoadSceneMode.Single);
     }
 }
